137cdfb394ad4b700c9f1dd1f18cba9f729093b7
Matityahu <matthew2001@gmail.com>
2015-08-30 17:07:04 +0300

    no updates
    don't use , just a test

--- LibraryUI/main.cpp
+++ LibraryUI/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string>
+//#include "LibraryBE\Book.h"
+
 using namespace std;
+
 void main()
 {
-  cout << "Hello Staff!" << endl; 
-  cout << "Welcome to Library" << endl; 
+	cout << "Hello Staff!\nWelcome to the Library" << endl;
+	cout << endl << "Please choose an option below:" << endl
+		<< " 1. Borrow book\n 2. Return Book"<<endl;
 
-  system("pause");
+	int _choice;
+	cin >> _choice;
+
+	switch (_choice)
+	{
+	case 1://borrow book
+	{
+			   cout << "Which book would you like to borrow?" << endl;
+		break;
+	}
+	case 2://return book
+	{
+		cout << "Entered Return Book";
+		break;
+	}
+	default:
+		break;
+	}
+
+	system("pause");
+	return;
 }
